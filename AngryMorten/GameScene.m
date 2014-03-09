8fc7bac6327c97b32ed9df11e4df93ac47172ff7
Ruben Teijeiro <sources.list@gmail.com>
2014-03-09 22:26:23 +0100

    Code comment and reorder.

--- AngryMorten/GameScene.m
+++ AngryMorten/GameScene.m
@@ -32,65 +32,14 @@ static inline CGFloat ScalarRandomRange(CGFloat min, CGFloat max) {
     return self;
 }
 
+// *********************
+// Game related methods.
+// *********************
+
 -(void)startGame {
   [self loadBackground];
   [self loadGUI];
   [self loadPlayer];
-}
-
--(void)loadPlayer {
-  Player *player = [[Player alloc] initWithImageNamed:@"player1"];
-  player.position = CGPointMake(self.size.width / 2, player.size.height / 2);
-  player.spitAnimation = [self createSpitAnimation];
-  player.initialImage = [SKTexture textureWithImageNamed:@"player1"];
-  player.shootImage = [SKTexture textureWithImageNamed:@"player5"];
-  
-  [self addChild:player];
-}
-
--(SKAction *)createSpitAnimation {
-  // Spitting Animation.
-  NSMutableArray *textures = [NSMutableArray arrayWithCapacity:5];
-  
-  for (int i = 2; i <= 4; i++) {
-    NSString *textureName = [NSString stringWithFormat:@"player%d", i];
-    SKTexture *texture = [SKTexture textureWithImageNamed:textureName];
-    [textures addObject:texture];
-  }
-  
-  SKAction *animation = [SKAction animateWithTextures:textures timePerFrame:0.1];
-  
-  return animation;
-}
-
--(void)spawnEnemy {
-  int randomInt = arc4random_uniform(2);
-  switch (randomInt) {
-    case 0:
-      [self spawnCar];
-      break;
-      
-    case 1:
-      [self spawnBike];
-      break;
-      
-    default:
-      break;
-  }
-}
-
--(void)spawnCar {
-  Enemy *car = [Enemy new];
-  car = [car initWithImageNamed:@"car"];
-  car.position = [self randomVehiclePosition:car.size.height];
-  [self addChild:car];
-}
-
--(void)spawnBike {
-  Enemy *bike = [Enemy new];
-  bike = [bike initWithImageNamed:@"bike"];
-  bike.position = [self randomVehiclePosition:bike.size.height];
-  [self addChild:bike];
 }
 
 -(void)loadBackground {
@@ -212,10 +161,75 @@ static inline CGFloat ScalarRandomRange(CGFloat min, CGFloat max) {
   _timeLastUpdate = currentTime;
   _spawnTime += elapsed;
   
+  // Spawn a new enemy every second.
   if (_spawnTime > 1.0f) {
     [self spawnEnemy];
     _spawnTime = 0.0f;
   }
+}
+
+// ***********************
+// Player related methods.
+// ***********************
+
+-(void)loadPlayer {
+  Player *player = [[Player alloc] initWithImageNamed:@"player1"];
+  player.position = CGPointMake(self.size.width / 2, player.size.height / 2);
+  player.spitAnimation = [self createSpitAnimation];
+  player.initialImage = [SKTexture textureWithImageNamed:@"player1"];
+  player.shootImage = [SKTexture textureWithImageNamed:@"player5"];
+  
+  [self addChild:player];
+}
+
+-(SKAction *)createSpitAnimation {
+  // Spitting Animation.
+  NSMutableArray *textures = [NSMutableArray arrayWithCapacity:5];
+  
+  for (int i = 2; i <= 4; i++) {
+    NSString *textureName = [NSString stringWithFormat:@"player%d", i];
+    SKTexture *texture = [SKTexture textureWithImageNamed:textureName];
+    [textures addObject:texture];
+  }
+  
+  SKAction *animation = [SKAction animateWithTextures:textures timePerFrame:0.1];
+  
+  return animation;
+}
+
+// **********************
+// Enemy related methods.
+// **********************
+
+-(void)spawnEnemy {
+  // Spawn random enemies.
+  int randomInt = arc4random_uniform(2);
+  switch (randomInt) {
+    case 0:
+      [self spawnCar];
+      break;
+      
+    case 1:
+      [self spawnBike];
+      break;
+      
+    default:
+      break;
+  }
+}
+
+-(void)spawnCar {
+  Enemy *car = [Enemy new];
+  car = [car initWithImageNamed:@"car"];
+  car.position = [self randomVehiclePosition:car.size.height];
+  [self addChild:car];
+}
+
+-(void)spawnBike {
+  Enemy *bike = [Enemy new];
+  bike = [bike initWithImageNamed:@"bike"];
+  bike.position = [self randomVehiclePosition:bike.size.height];
+  [self addChild:bike];
 }
 
 -(CGPoint)randomVehiclePosition:(float)vehicleHeight {
